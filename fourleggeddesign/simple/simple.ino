ef8de3fbf8f946baebbaec484252fa9aac95c2fc
egronstad <gronstad@usc.edu>
2022-03-21 18:11:20 -0700

    something with simpl.io lol I don't remember

--- fourleggeddesign/simple/simple.ino
+++ fourleggeddesign/simple/simple.ino
@@ -1,7 +1,8 @@
+#include <Servo.h>
+
 const int SERVO_PIN = 9;
 const int SOLENOID_PIN = 10;
 Servo servo;
-
 
 void setup() {
     servo.attach(SERVO_PIN);
@@ -11,10 +12,10 @@ void setup() {
 }
 
 void loop() {
-    delay(10000);
+    delay(4000);
     digitalWrite(SOLENOID_PIN, HIGH);
     servo.write(180);
-    delay(1000);
+    delay(2000);
     digitalWrite(SOLENOID_PIN, LOW);
     servo.write(0);
 }
